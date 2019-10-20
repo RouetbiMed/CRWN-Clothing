a1c84e520f14348b183bc4e4ee4b955bc84aee88
mrouetbi <mrouetbi@anypli.com>
2019-10-20 12:07:28 +0100

    sign up component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from './components/pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-import {auth} from "./firebase/firebase.utils";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 
 import './App.css';
 
@@ -23,9 +23,22 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
-            console.log(user);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        }
+                    }, () => console.log(this.state));
+                });
+
+            } else {
+                this.setState({currentUser: userAuth});
+            }
         });
     }
 
