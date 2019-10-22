6ec70a1e3b72a14ed2edef215d8e8a763640e686
mrouetbi <mrouetbi@anypli.com>
2019-10-22 21:53:40 +0100

    regroup items

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import {cartActionTypes} from "./cart.types";
+import {addItemToCart} from "./cart.utils";
 
 const INITIAL_STATE = {
     hidden: true,
@@ -10,7 +11,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case cartActionTypes.TOGGLE_CART_HIDDEN:
             return {...state, hidden: !state.hidden};
         case cartActionTypes.ADD_ITEM:
-            return {...state, cartItems: [...state.cartItems, action.payload]};
+            return {...state, cartItems: addItemToCart(state.cartItems, action.payload)};
         default:
             return state;
     }
