d5c21a11c281f57ba26c944ae1f61b04726013f6
mrouetbi <mrouetbi@anypli.com>
2019-10-26 15:12:55 +0100

    integrate redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import {combineReducers} from "redux";
+import {persistReducer} from "redux-persist"; // redux-persist config
+import storage from 'redux-persist/lib/storage'; // redux-persist config
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persisConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}; // redux-persist config
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
-});
+});  // redux-persist config
+
+export default persistReducer(persisConfig, rootReducer); // redux-persist config
