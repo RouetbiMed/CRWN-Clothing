2586c396716d064202e4cb36358b2ee055a0b49c
mrouetbi <mrouetbi@anypli.com>
2019-10-26 11:32:31 +0100

    checkout component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './components/pages/checkout/checkout.component';
 
 import {setCurrentUser} from './redux/user/user.actions';
 import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
@@ -49,6 +50,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route exact path='/shop' component={ShopPage}/>
+                    <Route exact path='/checkout' component={CheckoutPage}/>
                     <Route exact path='/signin'
                            render={() => this.props.currentUser ? (<Redirect to='/'/>) : <SignInAndSignUpPage/>}/>
                 </Switch>
